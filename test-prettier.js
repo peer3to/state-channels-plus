e8d7c805349184289434ea7ca70eabe359dc902f
luke <daiagi@gmail.com>
2025-04-06 10:56:57 +0200

    Testing prettier pre-commit hook

--- test-prettier.js
+++ test-prettier.js
@@ -1,2 +1,7 @@
-function test(  ) {return     "This is unformatted";}
-const x = { a:1,b:   2, c: 3 };
+function test() {
+    return "This is unformatted";
+}
+const x = { a: 1, b: 2, c: 3 };
+function badFormat() {
+    return "bad formatting";
+}
