50616e2c098d562c0ad4c65318aba5904bd32572
MrishoLukamba <abdulrazzaqlukamba@gmail.com>
2025-04-24 18:38:06 +0300

    fixed errors and compiling

--- test/disputes/DisputesContract.test.ts
+++ test/disputes/DisputesContract.test.ts
@@ -3,7 +3,6 @@ import { BigNumberish } from "ethers";
 import EvmStateMachine from "@/evm/EvmStateMachine";
 import { MathStateMachine } from "@typechain-types";
 
-
 describe("DisputesManagerContract", function () {
     let peerOne: any;
     let peerTwo: any;
@@ -11,72 +10,39 @@ describe("DisputesManagerContract", function () {
     let DisputeManagerInstance: any;
     this.beforeAll(async function () {
         const [owner] = await ethers.getSigners();
-        const DisputesContract = await ethers.getContractFactory("DisputesContract");
-        const disputesContract = await DisputesContract.deploy();
     });
 
     // Block Fraud Proofs
-    it("should handle double signing fraud proofs", async function () {
-    
-    });
+    it("should handle double signing fraud proofs", async function () {});
 
-    it("should handle invalid state transition fraud proofs", async function () {
-    
-    });
+    it("should handle invalid state transition fraud proofs", async function () {});
 
-    it("should handle state transition out of gas fraud proofs", async function () {
-    
-    });
+    it("should handle state transition out of gas fraud proofs", async function () {});
 
-    it("should handle empty block fraud proofs", async function () {
-    
-    });
-    
+    it("should handle empty block fraud proofs", async function () {});
+
     // Dispute Fraud Proofs
-    it("should handle not latest state fraud proofs", async function () {
-    
-    });
+    it("should handle not latest state fraud proofs", async function () {});
 
-    it("should handle invalid dispute fraud proofs", async function () {
-    
-    });
+    it("should handle invalid dispute fraud proofs", async function () {});
 
-    it("should handle dipsute out of gas", async function () {
-    
-    });
-    
-    it("should handle invalid output state fraud proofs", async function () {
-    
-    });
-    
-    it("should handle invalid state proofs", async function () {
-    
-    });
+    it("should handle dipsute out of gas", async function () {});
 
-    it("should handle invalid exit channel block fraud proofs", async function () {
-    
-    });
+    it("should handle invalid output state fraud proofs", async function () {});
+
+    it("should handle invalid state proofs", async function () {});
+
+    it("should handle invalid exit channel block fraud proofs", async function () {});
 
     // recurrsive disputes
-    it("should handle invalid recursive dispute proofs", async function () {
-    
-    });
+    it("should handle invalid recursive dispute proofs", async function () {});
 
-    it("should handle invalid previous recursive dispute proofs", async function () {
-    
-    });
+    it("should handle invalid previous recursive dispute proofs", async function () {});
 
     // Timeout Fraud Proofs
-    it("should handle timeout threshold fraud proofs", async function () {        
+    it("should handle timeout threshold fraud proofs", async function () {});
 
-    });
+    it("should handle timeout prior invalid fraud proofs", async function () {});
 
-    it("should handle timeout prior invalid fraud proofs", async function () {        
-
-    });
-    
-    it("should handle timeout participant no next fraud proofs", async function () {        
-
-    });
-
+    it("should handle timeout participant no next fraud proofs", async function () {});
 });
