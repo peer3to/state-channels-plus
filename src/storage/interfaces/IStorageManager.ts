31daaa7ce97259cc642c6107c6f5bd8db4fa57c0
krayt78 <ludovic.domingues96@gmail.com>
2025-06-05 18:11:44 +0200

    removed unused interfaces, cleaned storage manager

--- src/storage/interfaces/IStorageManager.ts
+++ src/storage/interfaces/IStorageManager.ts
@@ -1,18 +1,10 @@
-import { IBlockStorageModule } from "./IBlockStorage";
-import { IJoinChannelStorageModule } from "./IJoinChannelStorage";
-import { IExitChannelStorageModule } from "./IExitChannelStorage";
-import { IStateSnapshotStorageModule } from "./IStateSnapshotStorage";
 import {
     BlockStruct,
     StateSnapshotStruct
 } from "@typechain-types/contracts/V1/DataTypes";
 import { BytesLike } from "ethers";
 
-export interface IStorageModule
-    extends IBlockStorageModule,
-        IJoinChannelStorageModule,
-        IExitChannelStorageModule,
-        IStateSnapshotStorageModule {
+export interface IStorageModule {
     getLatestBlock(): {
         stateSnapshot: StateSnapshotStruct;
         block: BlockStruct;
