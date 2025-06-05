a280bad8b889fda944b65d66d18c2e6dd577f971
krayt78 <ludovic.domingues96@gmail.com>
2025-06-05 17:48:30 +0200

    added overrides and lots of functions to BlockStorage

--- src/storage/interfaces/IBlockStorage.ts
+++ src/storage/interfaces/IBlockStorage.ts
@@ -3,8 +3,23 @@ import {
     SignedBlockStruct
 } from "@typechain-types/contracts/V1/DataTypes";
 import { BlockHash } from "@/types/storage";
+import { BlockConfirmation } from "@/agreementManager/types";
 
 export interface IBlockStorageModule {
+    insertBlock(signedBlock: SignedBlockStruct): void;
+    insertBlock(
+        signedBlock: SignedBlockStruct,
+        blockHash: BlockHash,
+        fork: number,
+        height: number
+    ): void;
+    insertBlock(blockConfirmation: BlockConfirmationStruct): void;
+    insertBlock(
+        blockConfirmation: BlockConfirmationStruct,
+        blockHash: BlockHash,
+        fork: number,
+        height: number
+    ): void;
     getPreviousBlockHash(
         forkCnt: number,
         transactionCnt: number
