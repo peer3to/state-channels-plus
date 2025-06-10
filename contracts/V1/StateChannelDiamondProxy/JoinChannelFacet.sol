d032c19879e9ce09faacda0c89df2760590c1f12
daiagi <daiagi@gmail.com>
2025-06-10 10:28:41 +0700

    singature over disputeAudit Bundle is not the responsibilty of join channel facet

--- contracts/V1/StateChannelDiamondProxy/JoinChannelFacet.sol
+++ contracts/V1/StateChannelDiamondProxy/JoinChannelFacet.sol
@@ -11,12 +11,12 @@ contract JoinChannelFacet is StateChannelCommon {
      *
      * @param channelId The channel identifier
      * @param joinChannelConfirmations Array of join channel confirmations to process
-     * @param disputeAuditBundleConfirmation Signed dispute audit bundle to create new fork commitment
+     * @param dispute  to create new fork commitment
      */
     function joinChannel(
         bytes32 channelId,
         JoinChannelConfirmation[] memory joinChannelConfirmations,
-        DisputeAuditBundleConfirmation memory disputeAuditBundleConfirmation
+        Dispute memory dispute
     ) external {
         if (!isSnapshotLatest(channelId)) {
             revert ErrorSnapshotNotLatest();
@@ -80,19 +80,6 @@ contract JoinChannelFacet is StateChannelCommon {
             }
         }
 
-        // 3. Filter by joining participant signatures on dispute
-        for (uint256 i = 0; i < joinChannels.length; i++) {
-            if (!isValid[i]) continue; // Skip already invalid ones
-
-            bool hasValidSignature =
-                _didJoinerApproveDisputeAuditBundle(joiningParticipants[i], disputeAuditBundleConfirmation);
-
-            if (!hasValidSignature) {
-                isValid[i] = false;
-                validCount--;
-            }
-        }
-
         // Terminate early if no valid join channels
         require(validCount > 0, ErrorNoValidJoinChannels());
 
@@ -121,37 +108,13 @@ contract JoinChannelFacet is StateChannelCommon {
         _updatePendingParticipants(channelId, joinChannelBlock);
 
         // 7. Create dispute - this handles remaining verification and state updates
-        DisputeAuditBundle memory disputeAuditBundle = abi.decode(
-            disputeAuditBundleConfirmation.signedDisputeAuditBundle.encodedDisputeAuditBundle, (DisputeAuditBundle)
-        );
-        AStateChannelManagerProxy(address(this)).createDispute(disputeAuditBundle.dispute);
+        AStateChannelManagerProxy(address(this)).createDispute(dispute);
 
         // 8. Emit events
         emit JoinChannelProcessed(channelId, joinChannelBlock, block.timestamp);
     }
 
     // ############### Verification Functions ###############
-
-    /**
-     * @dev Checks if a joining participant has a valid signature on the dispute
-     */
-    function _didJoinerApproveDisputeAuditBundle(
-        address joiningParticipant,
-        DisputeAuditBundleConfirmation memory disputeAuditBundleConfirmation
-    ) internal pure returns (bool) {
-        // Find the signature from this participant
-        for (uint256 j = 0; j < disputeAuditBundleConfirmation.signatures.length; j++) {
-            address recoveredSigner = StateChannelUtilLibrary.retriveSignerAddress(
-                disputeAuditBundleConfirmation.signedDisputeAuditBundle.encodedDisputeAuditBundle,
-                disputeAuditBundleConfirmation.signatures[j]
-            );
-
-            if (recoveredSigner == joiningParticipant) {
-                return true;
-            }
-        }
-        return false;
-    }
 
     // ############### Processing Functions ###############
 
