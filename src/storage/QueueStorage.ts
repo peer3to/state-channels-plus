303517bcbb51404f79c8406e08e91f5a9f9eb39a
daiagi <daiagi@gmail.com>
2025-07-15 15:18:32 +0700

    oops - restore isBlockQueued

--- src/storage/QueueStorage.ts
+++ src/storage/QueueStorage.ts
@@ -85,6 +85,10 @@ export class QueueStorage {
         return blockConfirmations;
     }
 
+    isBlockQueued(hash: Hash): boolean {
+        return this.queuedBlocks.has(hash);
+    }
+
     // ====================================
     // PRIVATE HELPERS
     // ====================================
