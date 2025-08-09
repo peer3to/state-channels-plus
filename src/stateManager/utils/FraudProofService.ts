81d5187129a1c86648b8323e2f3e8db6806972bd
daiagi <daiagi@gmail.com>
2025-08-09 13:40:17 +0700

    store fraud proofs

--- src/stateManager/utils/FraudProofService.ts
+++ src/stateManager/utils/FraudProofService.ts
@@ -6,7 +6,12 @@ import {
     BlockInvalidStateTransitionProofStruct,
     InvalidTimestampProofStruct
 } from "@typechain-types/contracts/V1/types/FraudProofTypes";
+import { FraudProofStruct } from "@typechain-types/contracts/V1/types/ProofTypes";
 import { ZeroHash } from "ethers";
+import { Address, Hash, Signature } from "@/types/types";
+import { Codec, FraudStruct } from "@/utils/Codec";
+import { SignatureUtils } from "@/utils";
+import { FraudProofType } from "@/types";
 
 // ────────────────────── FRAUD PROOF SERVICE ─────────────────────
 
@@ -22,7 +27,7 @@ export default class FraudProofService {
     createInvalidStateTransitionProof(
         coordinates: BlockCoordinates,
         signedBlock: SignedBlockStruct
-    ): BlockInvalidStateTransitionProofStruct {
+    ): Hash {
         let prevSignedBlock: SignedBlockStruct | undefined;
         let prevStateSnapshot: StateSnapshot;
         const previousBlockOrSnapshot =
@@ -43,7 +48,7 @@ export default class FraudProofService {
             prevStateSnapshot = previousBlockOrSnapshot.stateSnapshot!;
         }
 
-        return {
+        const proof: BlockInvalidStateTransitionProofStruct = {
             invalidBlock: signedBlock,
             previousBlock: prevSignedBlock,
             previousBlockStateSnapshot: prevStateSnapshot.toStruct(),
@@ -52,6 +57,16 @@ export default class FraudProofService {
                     prevStateSnapshot.stateMachineStateHash
                 )!
         };
+
+        const participant = SignatureUtils.getSignerAddress(
+            signedBlock.encodedBlock,
+            signedBlock.signature as Signature
+        );
+
+        return this.storeFraudProof(participant, {
+            type: FraudProofType.BlockInvalidStateTransition,
+            struct: proof
+        });
     }
 
     /**
@@ -60,7 +75,7 @@ export default class FraudProofService {
     createInvalidTimestampProof(
         coordinates: BlockCoordinates,
         signedBlock: SignedBlockStruct
-    ): InvalidTimestampProofStruct {
+    ): Hash {
         let prevSignedBlock: SignedBlockStruct | undefined;
         let prevStateSnapshot: StateSnapshot;
         const previousBlockOrSnapshot =
@@ -77,24 +92,57 @@ export default class FraudProofService {
                 )!;
         } else {
             // Height === 0 case - we have genesis state snapshot
-            prevSignedBlock = NULL as unknown as SignedBlockStruct;
+            prevSignedBlock = ZeroHash as unknown as SignedBlockStruct;
             prevStateSnapshot = previousBlockOrSnapshot.stateSnapshot!;
         }
 
-        return {
+        const proof: InvalidTimestampProofStruct = {
             invalidBlock: signedBlock,
             previousBlock: prevSignedBlock,
             previousStateSnapshot: prevStateSnapshot.toStruct()
         };
+
+        const participant = SignatureUtils.getSignerAddress(
+            signedBlock.encodedBlock,
+            signedBlock.signature as Signature
+        );
+
+        return this.storeFraudProof(participant, {
+            type: FraudProofType.InvalidTimestamp,
+            struct: proof
+        });
     }
 
     createDoubleSignProof(
         conflictingSignedBlock: SignedBlockStruct,
         signedBlock: SignedBlockStruct
-    ): BlockDoubleSignProofStruct {
-        return {
+    ): Hash {
+        const proof: BlockDoubleSignProofStruct = {
             block1: conflictingSignedBlock,
             block2: signedBlock
         };
+
+        const participant1 = SignatureUtils.getSignerAddress(
+            conflictingSignedBlock.encodedBlock,
+            conflictingSignedBlock.signature as Signature
+        );
+
+        return this.storeFraudProof(participant1, {
+            type: FraudProofType.BlockDoubleSign,
+            struct: proof
+        });
+    }
+
+    private storeFraudProof(
+        participant: Address,
+        proof: { type: FraudProofType; struct: FraudStruct }
+    ): Hash {
+        const fraudProof: FraudProofStruct = {
+            proofType: proof.type,
+            participant,
+            encodedProof: Codec.encode(proof.struct, proof.type)
+        };
+
+        return this.storage.fraudProofs.storeFraudProof(fraudProof);
     }
 }
