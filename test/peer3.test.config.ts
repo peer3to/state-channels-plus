da224772929d058a44634c3c5275a7598b8716ce
Adrian Cirstea <adrianioan.cirstea@gmail.com>
2025-10-10 00:08:22 +0300

    added default values for test

--- test/peer3.test.config.ts
+++ test/peer3.test.config.ts
@@ -8,9 +8,9 @@ const testConfig: Partial<Config> = {
     DEBUG_RPC: false,
     DEBUG_CHANNEL_CONTRACT: false,
     DEBUG_LOCAL_TRANSPORT: true,
-    RATE_LIMIT_ENABLED: false,
-    RATE_LIMIT_BYTES_PER_SECOND: 0,
-    RATE_LIMIT_BURST_SIZE: 0
+    RATE_LIMIT_ENABLED: true,
+    RATE_LIMIT_BYTES_PER_SECOND: 10 * 1024 * 1024, // 10 MB/s
+    RATE_LIMIT_BURST_SIZE: 20 * 1024 * 1024 // 20 MB burst
 };
 
 export default testConfig;
