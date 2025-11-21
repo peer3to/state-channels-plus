fef4b2baebaa57a786357b3cbc87c2ac6b5a8667
Luka <lukastanisic2@gmail.com>
2025-11-21 14:49:13 +0100

    Complex types for staticCall

--- contracts/Counter.sol
+++ contracts/Counter.sol
@@ -1,8 +1,12 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.28;
-
+struct ComplexType {
+  uint a;
+  uint b;
+}
 contract Counter {
   uint public x;
+  ComplexType public complex;
 
   event Increment(uint by);
 
@@ -16,5 +20,9 @@ contract Counter {
     require(by > 0, "incBy: increment should be positive");
     x += by;
     emit Increment(by);
+  }
+  function setComplex(uint a, uint b) public returns (ComplexType memory) {
+    complex = ComplexType(a, b);
+    return complex;
   }
 }
--- test/StaticCall.ts
+++ test/StaticCall.ts
@@ -1,5 +1,6 @@
 import { expect } from "chai";
 import { network } from "hardhat";
+import { ComplexTypeStructOutput } from "../types/ethers-contracts/Counter.js";
 
 const { ethers } = await network.connect();
 
@@ -19,5 +20,25 @@ describe("StaticCall", function () {
     await counter.inc();
     value = await counter.x();
     expect(value).to.equal(2n);
+  });
+
+  it("StaticCall handles complex return type", async function () {
+    const counter = await ethers.deployContract("Counter");
+
+    const initialComplex = await counter.complex();
+    expect(initialComplex.a).to.equal(0n);
+    expect(initialComplex.b).to.equal(0n);
+    const staticResult = await counter.setComplex.staticCall(10n, 20n);
+    expect(staticResult.a).to.equal(10n);
+    expect(staticResult.b).to.equal(20n);
+
+    const afterStaticCall = await counter.complex();
+    expect(afterStaticCall.a).to.equal(0n);
+    expect(afterStaticCall.b).to.equal(0n);
+
+    await counter.setComplex(5n, 6n);
+    const updatedComplex = await counter.complex();
+    expect(updatedComplex.a).to.equal(5n);
+    expect(updatedComplex.b).to.equal(6n);
   });
 });
