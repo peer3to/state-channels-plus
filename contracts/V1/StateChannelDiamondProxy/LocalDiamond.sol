065fd4c049eacb9d7eff2b649a4c06a9edd3c6ea
Luka <lukastanisic2@gmail.com>
2026-01-25 13:21:51 +0100

    some logs

--- contracts/V1/StateChannelDiamondProxy/LocalDiamond.sol
+++ contracts/V1/StateChannelDiamondProxy/LocalDiamond.sol
@@ -346,23 +346,21 @@ contract LocalDiamond is StateChannelManagerProxy {
     // ========== Override for debugging - Browser compatible console logs ==========
 
     function isBlockAuthentic(SignedBlock memory _block) public view override returns (bool) {
-        console.log("========================================");
-        console.log("***LocalDiamond isBlockAuthentic CALLED ***");
-        console.log("========================================");
-
         // try decode block
         bytes memory data = abi.encodeCall(this.decodeBlock, (_block.encodedBlock));
         (bool success, bytes memory encodedBlock) = address(this).staticcall(data);
         if (!success) {
+            console.log("isBlockAuthentic - false - 1");
             return false;
         }
         Block memory decodedBlock = abi.decode(encodedBlock, (Block));
         (address signer, bool isValid) =
             UtilityFacet(utilityFacetAddress).retrieveSignerAddress(encodedBlock, _block.signature);
         if (signer != decodedBlock.transaction.header.participant || !isValid) {
+            console.log("isBlockAuthentic - false - 2");
             return false;
         }
-
+        console.log("isBlockAuthentic - true");
         return true;
     }
 }
