34e743f0bb2282c1ddf65f1504d4a3665e28f073
daiagi <daiagi@gmail.com>
2026-08-27 23:35:25 +0700

    add per-realm log flush bus

--- src/utils/logging/index.ts
+++ src/utils/logging/index.ts
@@ -1,5 +1,6 @@
 import type { CreateLoggerOptions } from "./createLoggerTypes";
 import { decodeLogs, decompressFromBase64 } from "./logEncoder";
+import { LogFlushBus, realmLogFlushBus } from "./LogFlushBus";
 import type {
     ExclusiveLoggerContext,
     SharedLoggerContext,
@@ -17,4 +18,14 @@ export type {
     LoggerPerformanceMonitorOptions,
     CreateLoggerOptions
 };
-export { decodeLogs, decompressFromBase64 };
+export { decodeLogs, decompressFromBase64, LogFlushBus, realmLogFlushBus };
+export type {
+    FlushId,
+    LogControlMessage,
+    LogControlPort,
+    LogFlushResult,
+    LogPortHandle,
+    LogRemoteRealm
+} from "./logControl";
+export type { LogThreadName } from "./Logger";
+export type { LogUploadOutcome } from "./LogUploader";
